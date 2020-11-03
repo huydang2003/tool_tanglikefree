8a2201566d3d1e4bca483939a270383464e86355
huydang2003 <huyhuydang2k3@gmail.com>
2020-11-03 13:31:06 +0700

    'ds'

--- bot.py
+++ bot.py
@@ -13,7 +13,8 @@ class Tool_Tanglikefree():
 		self.password = password
 		self.cookie = None
 		self.access_token = None
-		self.headers = None
+		self.headers_fb = None
+		self.headers_tlf = None
 		self.info = {}
 
 	def login_tlf(self):
@@ -23,7 +24,7 @@ class Tool_Tanglikefree():
 		data = res.json()
 		if data['error'] == False:
 			self.access_token = data['data']['access_token']
-			self.headers = {
+			self.headers_tlf = {
 				'Host': 'tanglikefree.com',
 				'Referer': 'https://tanglikefree.com/makemoney',
 				'Sec-Fetch-Dest': 'empty',
@@ -62,7 +63,7 @@ class Tool_Tanglikefree():
 		return ''
 
 	def get_token(self):
-		headers = {
+		self.headers_fb = {
 			'authority': 'mbasic.facebook.com',
 			'upgrade-insecure-requests': '1',
 			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -71,18 +72,13 @@ class Tool_Tanglikefree():
 			'accept-language': 'en-US,en;q=0.9',
 			'user_agent': 'Mozilla/5.0 (Linux; Android) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36'
 		}
-		headers['cookie'] = self.cookie
+		self.headers_fb['cookie'] = self.cookie
 		url = 'https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed'
-		res = self.ses.get(url, headers=headers)
+		res = self.ses.get(url, headers=self.headers_fb)
 		token = re.findall(r'accessToken\\":\\"(.*?)\\', res.text)
 		if token != []: token = token[0]
 		else: token = ''
 		return token
-
-	def save_file_json(self, path_input, data):
-		f = open(path_input, 'w', encoding='utf8')
-		json.dump(data, f, ensure_ascii=False, indent=4)
-		f.close()
 
 	def creat_backup(self, token):
 		params = {'access_token': token}
@@ -90,27 +86,22 @@ class Tool_Tanglikefree():
 		res = self.ses.get(url, params=params)
 		data = res.json()
 		path_data = f'nicks/{data["name"]}_{data["id"]}'
-		if not os.path.exists(path_data): 
-			os.mkdir(path_data)
+		self.info['name'] = data["name"]
+		if not os.path.exists(path_data): os.mkdir(path_data)
 		path_output = f'{path_data}/info.json'
-		self.save_file_json(path_output, data)
-		url = 'https://graph.facebook.com/me?fields=friends'
-		res = self.ses.get(url, params=params)
-		data = res.json()
-		data = data['friends']['data']
-		path_output = f'{path_data}/list_friend.json'
-		self.save_file_json(path_output, data)
-
+		f = open(path_output, 'w', encoding='utf8')
+		json.dump(data, f, ensure_ascii=False, indent=4)
+		f.close()
 
 	def get_post(self):
-		headers = self.headers
+		headers = self.headers_tlf
 		url = 'https://tanglikefree.com/api/auth/Post/getpost'
 		res = self.ses.get(url, headers=headers)
 		list_post = res.json()
 		return list_post
 
 	def get_request(self):
-		headers = self.headers
+		headers = self.headers_tlf
 		url = 'https://tanglikefree.com/api/auth/creat_request'	
 		res = self.ses.get(url, headers=headers)
 		data = res.json()
@@ -118,24 +109,33 @@ class Tool_Tanglikefree():
 		return requests
 
 	def submit_post(self, idpost, request_id):
+		headers = self.headers_tlf
 		payload = {'idpost': idpost, 'request_id': request_id}
-		headers = self.headers
 		url = 'https://tanglikefree.com/api/auth/Post/submitpost'
 		res = self.ses.post(url, data=payload, headers=headers)
-		data = res.json()
+		data = res.json()	
 		if data['error']==False:
 			return True
 		else: return False
 
-	def like_post(self, token, idpost):
-		params = {'access_token': token}
-		url = f'https://graph.facebook.com/{idpost}/likes'
-		res = self.ses.post(url, params=params)
-		data = res.json()
-		if data == True: return 1
-		else:
-			if data['error']['code'] != 368: return 0
-			else: return 2
+	def like_post(self, idpost):
+		link = 'https://mbasic.facebook.com/reactions/picker/?is_permalink=1&ft_id='+idpost
+		headers = self.headers_fb
+		res = self.ses.get(link, headers=headers)
+		soup = BeautifulSoup(res.content, 'html.parser')
+		soup = soup.body.find(id='root')
+		list_li = soup.find_all('li')
+		if list_li==[]: return 0	
+		url = list_li[0].a.get('href')
+		link = 'https://mbasic.facebook.com' + url
+		try: self.ses.get(link, headers=headers, timeout=2)
+		except: pass
+		return 1
+
+	def check_cookie(self):
+		token = self.get_token()
+		if token=='': return False
+		else: return True
 
 # cookie die 0, hoan thanh mot vong lap 1,  het nv 2, block 3
 
@@ -147,6 +147,7 @@ class Tool_Tanglikefree():
 		self.creat_backup(token)
 		coin = self.info['VND']
 		cout = 0
+		cout_failed = 0
 		while True:
 			while True:
 				list_post = self.get_post()
@@ -157,26 +158,34 @@ class Tool_Tanglikefree():
 			for post in list_post:
 				idpost = post['idpost']
 				request_id = self.get_request()
-				check = self.like_post(token, idpost)
+				check = self.like_post(idpost)
 				if check==0:
-					print('[Block like]')
-					return 3
-				elif check==2:
-					print('>>>make failed!!!')
-					token = self.get_token()
-					if token=='':
+					print('>>>link error!!!')	
+					check = self.check_cookie()
+					if check == False:
 						print(">>>Cookie die!!!")
 						return 0
+
 				else:
 					ck = self.submit_post(idpost, request_id)
-					if ck!=True: continue
-					cout += 1
-					coin += 40
-					print(f"  >{cout}<|{idpost}|>+40<|{coin} coin", end=' ')
-					if cout>=loop: return 1
-					s = random.randint(delay[0], delay[1])
-					print(f">>> wait {s}s")
-					sleep(s)
+					if ck!=True:
+						cout_failed+=1
+						if cout_failed > 3:
+							check = self.check_cookie()
+							if check == True:
+								print("[Block like!!!]")
+								return 3
+							else:
+								print(">>>Cookie die!!!")
+								return 0		
+					else:
+						cout += 1
+						coin += 40
+						print(f"  >{cout}<|{idpost}|>+40<|{coin} coin", end=' ')
+						if cout>=loop: return 1
+						s = random.randint(delay[0], delay[1])
+						print(f">>> wait {s}s")
+						sleep(s)
 
 if __name__ == '__main__':
 	if not os.path.exists('nicks'): os.mkdir('nicks')
@@ -192,11 +201,12 @@ if __name__ == '__main__':
 	delay[0] = int(delay[0])
 	delay[1] = int(delay[1])
 
-	list_nick = open('list_nick.txt', 'r').read().split("\n")
+	list_nick = open('list_nick.txt', 'r', encoding='utf8').read().split("\n")
 	list_nick_out = []
 	max_job = {}
+	check_close = False
 	while True:
-		if len(list_nick_out) >= len(list_nick): break
+		if check_close: break
 		for nick in list_nick:
 			username = nick.split('|')[0]
 			if username not in max_job: max_job[username] = 0
@@ -207,7 +217,7 @@ if __name__ == '__main__':
 			check = tool.login_tlf()
 			if check == True:
 				print(">>>Login Success!!!")
-				print("\n>>>making:")
+				print(f"\n>>>making: {tool.info['name']}({tool.info['idfb']})")
 				check = tool.run_tool(loop, delay)
 
 				if check==0 or check==3: list_nick_out.append(username)
@@ -215,6 +225,10 @@ if __name__ == '__main__':
 				if max_job[username] >= sl:
 					print(f"\n[Nick {username} đã hoàn thành chỉ tiêu]")
 					list_nick_out.append(username)
+
+				if len(list_nick_out) >= len(list_nick):
+					check_close = True
+					break
 
 				print(f"\n[Chuyển nick sau {time_change}s]")
 				sleep(time_change)
